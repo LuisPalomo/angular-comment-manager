072d6d1d9f98db5065523a2557822bd1e2c3b21d
Luis Palomo <luispalomosiv@gmail.com>
2017-07-17 08:55:50 +0200

    Fixed some styles.

--- src/app/mocked-data.ts
+++ src/app/mocked-data.ts
@@ -19,24 +19,24 @@ export let comments: Comment[] = [
     id: '1',
     user: users[0],
     text: 'Hello Bruce!',
-    time: (new Date() + '').substring(16, 21)
+    time: new Date().toString()
   },
   {
     id: '2',
     user: users[1],
     text: 'Hi Chuck! What\'s up? ',
-    time: (new Date() + '').substring(16, 21)
+    time: new Date().toString()
   },
   {
     id: '3',
     user: users[0],
-    text: 'I\'ve always found that anything worth achieving will always have obstacles in the way and you\'ve got to have that drive and determination to overcome those obstacles on route to whatever it is that you want to accomplish',
-    time: (new Date() + '').substring(16, 21)
+    text: 'I\'ve always found that anything worth achieving will always have obstacles in the way and you\'ve got to have that drive and determination to overcome those obstacles on route to whatever it is that you want to accomplish.',
+    time: new Date().toString()
   },
   {
     id: '3',
     user: users[1],
-    text: 'Empty your mind, be formless, shapeless—like water',
-    time: (new Date() + '').substring(16, 21)
+    text: 'Don\'t get set into one form, adapt it and build your own, and let it grow, be like water. Empty your mind, be formless, shapeless—like water. If you put water into a cup, it becomes the cup. You put water into a bottle it becomes the bottle. You put it in a teapot it becomes the teapot. Now, water can flow or it can crash. Be water my friend.',
+    time: new Date().toString()
   }
 ];
